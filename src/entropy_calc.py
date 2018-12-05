fix output range bug for peb entropy calculation

src/entropy_calc.py
@@ -1,12 +1,22 @@
 import scipy.stats
 import torch
+import numpy as np
 
 class Entropy_Calc():
 
     @staticmethod
     def peb_entropy_calc(pebs):
-        pebs_list = torch.reshape(pebs, (-1, ))
-        entropy=scipy.stats.entropy(pebs_list)  # input probabilities to get the entropy 
+        pebs_list = pebs[0]
+        #pebs_list = torch.reshape(pebs, (-1, ))
+        max_val = torch.max(pebs_list)
+        min_val = torch.min(pebs_list)
+        unit = (max_val - min_val) / 999
+        occu_list = [0] * 1000
+        for val in pebs_list:
+            index = np.floor(((val - min_val) / unit).cpu().numpy()).astype(int)
+            occu_list[index] += 1
+        dist_list = [x / sum(occu_list) for x in occu_list]
+        entropy=scipy.stats.entropy(occu_list)  # input probabilities to get the entropy 
         return entropy
 
     @staticmethod

src/model_ed.py
@@ -72,9 +72,9 @@ class CNN_2_EDropout(nn.Module):
         
             pebs = torch.cat(peb_list, dim=0) # calc peb
             mask, retain_p = DropoutMask.mask(pebs) # calc mask
-            peb_entropy = Entropy_Calc.peb_entropy_calc(pebs) # calc pe
-            self.ed.entropy_list.append(peb_entropy)
-            self.ed.peek_peb = torch.max(pebs) # update peek pebss
+            peb_entropy = Entropy_Calc.peb_entropy_calc(pebs) # calc peb entropy val
+            self.ed.peb_entropy = peb_entropy
+            self.ed.peek_peb = torch.max(pebs) # update peek peb
             eb.use_eb(False, verbose=False)
 
         self.ed.train = self.training  # ugly code!